1dc1897d3f0a35ba1286da0d326a8c5b008ef267
Tom Levy <tomlevy93@gmail.com>
2020-03-18 23:47:02 +1300

    Add "Team members" field to registration form

--- webapp/src/Form/Type/UserRegistrationType.php
+++ webapp/src/Form/Type/UserRegistrationType.php
@@ -18,6 +18,7 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Form;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -119,6 +120,19 @@ class UserRegistrationType extends AbstractType
                     'constraints' => [
                         new NotBlank(),
                     ],
+                ]);
+        }
+
+        if ($this->config->get('show_team_members')) {
+            $builder
+                ->add('members', TextareaType::class, [
+                    'label' => false,
+                    'mapped' => false,
+                    'attr' => [
+                        'placeholder' => 'Team members (optional)',
+                    ],
+                    'required' => false,
+                    'mapped' => false,
                 ]);
         }
 
