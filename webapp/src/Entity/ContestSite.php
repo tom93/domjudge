f6e4b810366418a001469c4542b37379150bada3
Tom Levy <tomlevy93@gmail.com>
2020-04-06 02:29:54 +1200

    Add contest site property to team model

--- webapp/src/Entity/ContestSite.php
+++ webapp/src/Entity/ContestSite.php
@@ -2,6 +2,8 @@
 // based on TeamCategory.php
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use JMS\Serializer\Annotation as Serializer;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -51,6 +53,17 @@ class ContestSite extends BaseApiEntity
      * @Serializer\Exclude()
      */
     private bool $active = true;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Team", mappedBy="site")
+     * @Serializer\Exclude()
+     */
+    private Collection $teams;
+
+    public function __construct()
+    {
+        $this->teams = new ArrayCollection();
+    }
 
     public function __toString(): string
     {
@@ -104,5 +117,10 @@ class ContestSite extends BaseApiEntity
     public function getActive(): bool
     {
         return $this->active;
+    }
+
+    public function getTeams(): Collection
+    {
+        return $this->teams;
     }
 }
