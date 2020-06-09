170d2aa025ee6b1f8284dfa9aa52ff43a438a4e3
Tom Levy <tomlevy93@gmail.com>
2020-06-10 04:39:02 +1200

    backport: Add ConfigurationService shim to SecurityController and UserRegistrationType

--- webapp/src/Controller/SecurityController.php
+++ webapp/src/Controller/SecurityController.php
@@ -22,6 +22,14 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    // backport: use a shim for the ConfigurationService class (added in DOMjudge 7.3)
+    protected $config;
+    /** @required */
+    public function setConfig(\App\Service\ConfigurationService $config)
+    {
+        $this->config = $config;
+    }
+
     /**
      * @var DOMJudgeService
      */
--- webapp/src/Form/Type/UserRegistrationType.php
+++ webapp/src/Form/Type/UserRegistrationType.php
@@ -26,6 +26,14 @@ use Symfony\Component\Validator\Context\ExecutionContext;
 
 class UserRegistrationType extends AbstractType
 {
+    // backport: use a shim for the ConfigurationService class (added in DOMjudge 7.3)
+    protected $config;
+    /** @required */
+    public function setConfig(\App\Service\ConfigurationService $config)
+    {
+        $this->config = $config;
+    }
+
     /**
      * @var DOMJudgeService
      */
