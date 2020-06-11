ed0f4d73c1dd980b422d32a29732e1dd3fc88c6c
Tom Levy <tomlevy93@gmail.com>
2020-06-12 06:35:32 +1200

    backport: Add ConfigurationService shim to TeamController, PublicController, Team/ScoreboardController

--- webapp/src/Controller/Jury/TeamController.php
+++ webapp/src/Controller/Jury/TeamController.php
@@ -30,6 +30,14 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class TeamController extends BaseController
 {
+    // backport: use a shim for the ConfigurationService class (added in DOMjudge 7.3)
+    protected $config;
+    /** @required */
+    public function setConfig(\App\Service\ConfigurationService $config)
+    {
+        $this->config = $config;
+    }
+
     /**
      * @var EntityManagerInterface
      */
--- webapp/src/Controller/PublicController.php
+++ webapp/src/Controller/PublicController.php
@@ -27,6 +27,14 @@ use Symfony\Component\Routing\RouterInterface;
  */
 class PublicController extends BaseController
 {
+    // backport: use a shim for the ConfigurationService class (added in DOMjudge 7.3)
+    protected $config;
+    /** @required */
+    public function setConfig(\App\Service\ConfigurationService $config)
+    {
+        $this->config = $config;
+    }
+
     /**
      * @var DOMJudgeService
      */
