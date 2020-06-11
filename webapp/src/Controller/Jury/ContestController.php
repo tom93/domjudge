22d1938fca125d92d76f4b776df58881ea66a2f0
Tom Levy <tomlevy93@gmail.com>
2020-06-12 06:42:37 +1200

    backport: Add ConfigurationService shim to ContestController

--- webapp/src/Controller/Jury/ContestController.php
+++ webapp/src/Controller/Jury/ContestController.php
@@ -32,6 +32,14 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class ContestController extends BaseController
 {
+    // backport: use a shim for the ConfigurationService class (added in DOMjudge 7.3)
+    protected $config;
+    /** @required */
+    public function setConfig(\App\Service\ConfigurationService $config)
+    {
+        $this->config = $config;
+    }
+
     /**
      * @var EntityManagerInterface
      */
