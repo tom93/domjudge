32193cf9bb140da2486f0561221a5bcf038d5d39
Tom Levy <tomlevy93@gmail.com>
2020-06-13 16:07:50 +1200

    backport: Add ConfigurationService shim to UserController

--- webapp/src/Controller/Jury/UserController.php
+++ webapp/src/Controller/Jury/UserController.php
@@ -28,6 +28,14 @@ use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
  */
 class UserController extends BaseController
 {
+    // backport: use a shim for the ConfigurationService class (added in DOMjudge 7.3)
+    protected $config;
+    /** @required */
+    public function setConfig(\App\Service\ConfigurationService $config)
+    {
+        $this->config = $config;
+    }
+
     /**
      * @var EntityManagerInterface
      */
