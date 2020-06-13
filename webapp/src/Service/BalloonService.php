c4afa328ef5eaf55514a39a77e15530e1dd97d5e
Tom Levy <tomlevy93@gmail.com>
2020-06-13 17:01:48 +1200

    backport: Add ConfigurationService shim to BalloonService

--- webapp/src/Service/BalloonService.php
+++ webapp/src/Service/BalloonService.php
@@ -10,6 +10,14 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class BalloonService
 {
+    // backport: use a shim for the ConfigurationService class (added in DOMjudge 7.3)
+    protected $config;
+    /** @required */
+    public function setConfig(\App\Service\ConfigurationService $config)
+    {
+        $this->config = $config;
+    }
+
     /**
      * @var EntityManagerInterface
      */
