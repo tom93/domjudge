c33ba9ecbe41fa1fc3fb5565f30149f23928dbcc
Tom Levy <tomlevy93@gmail.com>
2020-07-24 08:00:27 +1200

    Speed up chown in Docker
    Recursive chown is slow in Docker because of the copy-on-write
    filesystem.[1]
    
    Avoid chown by creating files with correct ownership where possible.
    
    Where it is not possible (specifically "ADD domjudge.tar.gz" because
    the owner in the archive is arbitrary and can't be overridden[2]),
    optimise the case where the files already have the correct ownership.
    
    [1] https://github.com/docker/for-linux issue 388
    [2] https://github.com/moby/moby issue 35525

--- docker/domserver/build.sh
+++ docker/domserver/build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -eu
 
 cd /domjudge-src/domjudge*
-chown -R domjudge: .
 # If we used a local source tarball, it might not have been built yet
 sudo -u domjudge make dist
 sudo -u domjudge ./configure -with-baseurl=http://localhost/
