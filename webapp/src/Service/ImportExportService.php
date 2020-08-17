172db78abca68daf4834256c322670fc86c0e06b
Tom Levy <tomlevy93@gmail.com>
2020-08-18 05:12:54 +1200

    Add organization TSV import (non-standard format)

--- webapp/src/Service/ImportExportService.php
+++ webapp/src/Service/ImportExportService.php
@@ -488,6 +488,11 @@ class ImportExportService
     public function importTsv(string $type, UploadedFile $file, ?string &$message = null): int
     {
         $content = file($file->getRealPath());
+        if ($type === 'organizations-nzpc') {
+            // In this format the tsv should not contain a version line.
+            // Insert a fake version line so the code below works.
+            array_unshift($content, sprintf("%s\t%s\n", $type, '1'));
+        }
         // The first line of the tsv is always the format with a version number.
         // Currently, we hardcode version 1 because there are no others.
         $version = rtrim(array_shift($content));
@@ -506,6 +511,8 @@ class ImportExportService
         switch ($type) {
             case 'groups':
                 return $this->importGroupsTsv($content, $message);
+            case 'organizations-nzpc':
+                return $this->importOrganizationsNzpcTsv($content, $message);
             case 'teams':
                 return $this->importTeamsTsv($content, $message);
             case 'accounts':
@@ -655,6 +662,31 @@ class ImportExportService
         }
 
         return count($groupData);
+    }
+
+    /**
+     * Import organizations TSV (NZPC format).
+     */
+    protected function importOrganizationsNzpcTsv(array $content, string &$message = null): int
+    {
+        $organizationData = [];
+        $l                = 1;
+        $l--; // No version line
+        foreach ($content as $line) {
+            $l++;
+            $line = Utils::parseTsvLine(trim($line));
+            if (count($line) !== 2) {
+                $message = sprintf('cannot parse organization on line %d: expected 2 tab-separated values, found %d',
+                                   $l, count($line));
+                return -1;
+            }
+            $organizationData[] = [
+                'shortname' => @$line[0],
+                'name' => @$line[1],
+            ];
+        }
+
+        return $this->importOrganizationData($organizationData);
     }
 
     /**
