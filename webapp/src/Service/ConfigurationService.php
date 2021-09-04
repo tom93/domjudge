83692f337a135d07aed23c10023ee5f829e1d043
Tom Levy <tomlevy93@gmail.com>
2021-09-04 19:16:58 +1200

    backport: Add default value for upcoming configuration option 'team_members_required'

--- webapp/src/Service/ConfigurationService.php
+++ webapp/src/Service/ConfigurationService.php
@@ -57,6 +57,7 @@ class ConfigurationService
     protected $defaults = [
         'show_team_members' => true,
         'show_team_members_publicly' => true,
+        'team_members_required' => false,
         'show_teams_with_no_submissions' => true,
         'show_new_affiliation_option' => true,
         'show_user_emails' => true,
