908b2848ed921afbd57b83a4938af09abd1dd21a
Michael <14887731+vmcj@users.noreply.github.com>
2024-01-12 15:56:30 +0100

    cherry-pick: Create action for docker build scripts
    This cherry-picks the part that adds the namespace argument to
    build.sh (replacing our BUILD_REPOSITORY variable), with some manual
    modifications.

--- docker/build.sh
+++ docker/build.sh
@@ -6,16 +6,16 @@ then
 	export PS4='(${0}:${LINENO}): - [$?] $ '
 fi
 
-if [ "$#" -ne 1 ]
+if [ "$#" -eq 0 ] || [ "$#" -gt 2 ]
 then
-	echo "Usage: $0 domjudge-version"
+	echo "Usage: $0 domjudge-version <namespace>"
 	echo "	For example: $0 5.3.0"
+	echo "	or: $0 5.3.0 otherNamespace"
 	exit 1
 fi
 
 VERSION="$1"
-
-REPOSITORY="${BUILD_REPOSITORY-domjudge}"
+NAMESPACE="${2-domjudge}"
 
 if [ "${BUILD_DOWNLOAD_RELEASE-0}" != 0 ]; then
 URL=https://www.domjudge.org/releases/domjudge-${VERSION}.tar.gz
@@ -35,37 +35,37 @@ fi
 if [ "${BUILD_FROM_SOURCE-1}" != 0 ]; then
 echo "[..] Boostrapping..."
 tar c -C .. --exclude=./.git --exclude="./docker/*.tar.gz" . |
-	docker build --target=dist -t "$REPOSITORY/dist:${VERSION}" -f docker/domserver/Dockerfile.source -
-docker run --rm "$REPOSITORY/dist:${VERSION}" tar cz -C /domjudge-src domjudge > domjudge.tar.gz
-docker rmi --no-prune "$REPOSITORY/dist:${VERSION}"
+	docker build --target=dist -t "${NAMESPACE}/dist:${VERSION}" -f docker/domserver/Dockerfile.source -
+docker run --rm "${NAMESPACE}/dist:${VERSION}" tar cz -C /domjudge-src domjudge > domjudge.tar.gz
+docker rmi --no-prune "${NAMESPACE}/dist:${VERSION}"
 echo "[ok] Done boostrapping"
 fi
 
 if [ "${BUILD_DOMSERVER-1}" != 0 ]; then
 echo "[..] Building Docker image for domserver..."
-./build-domjudge.sh "$REPOSITORY/domserver:${VERSION}"
+./build-domjudge.sh "${NAMESPACE}/domserver:${VERSION}"
 echo "[ok] Done building Docker image for domserver"
 fi
 
 if [ "${BUILD_JUDGEHOST-1}" != 0 ]; then
 echo "[..] Building Docker image for judgehost using intermediate build image..."
-./build-judgehost.sh "$REPOSITORY/judgehost:${VERSION}"
+./build-judgehost.sh "${NAMESPACE}/judgehost:${VERSION}"
 echo "[ok] Done building Docker image for judgehost"
 fi
 
 if [ "${BUILD_DEFAULT_JUDGEHOST_CHROOT-0}" != 0 ]; then
 echo "[..] Building Docker image for judgehost chroot..."
-docker build -t "$REPOSITORY/default-judgehost-chroot:${VERSION}" -f judgehost/Dockerfile.chroot .
+docker build -t "${NAMESPACE}/default-judgehost-chroot:${VERSION}" -f judgehost/Dockerfile.chroot .
 echo "[ok] Done building Docker image for judgehost chroot"
 fi
 
 if [ "${BUILD_PRINT_HELP-0}" != 0 ]; then
-echo "All done. Image $REPOSITORY/domserver:${VERSION} and $REPOSITORY/judgehost:${VERSION} created"
+echo "All done. Image ${NAMESPACE}/domserver:${VERSION} and ${NAMESPACE}/judgehost:${VERSION} created"
 echo "If you are a DOMjudge maintainer with access to the domjudge organization on Docker Hub, you can now run the following command to push them to Docker Hub:"
-echo "$ docker push $REPOSITORY/domserver:${VERSION} && docker push $REPOSITORY/judgehost:${VERSION} && docker push $REPOSITORY/default-judgehost-chroot:${VERSION}"
+echo "$ docker push ${NAMESPACE}/domserver:${VERSION} && docker push ${NAMESPACE}/judgehost:${VERSION} && docker push ${NAMESPACE}/default-judgehost-chroot:${VERSION}"
 echo "If this is the latest release, also run the following command:"
-echo "$ docker tag $REPOSITORY/domserver:${VERSION} $REPOSITORY/domserver:latest && \
-docker tag $REPOSITORY/judgehost:${VERSION} $REPOSITORY/judgehost:latest && \
-docker tag $REPOSITORY/default-judgehost-chroot:${VERSION} $REPOSITORY/default-judgehost-chroot:latest && \
-docker push $REPOSITORY/domserver:latest && docker push $REPOSITORY/judgehost:latest && docker push $REPOSITORY/default-judgehost-chroot:latest"
+echo "$ docker tag ${NAMESPACE}/domserver:${VERSION} ${NAMESPACE}/domserver:latest && \
+docker tag ${NAMESPACE}/judgehost:${VERSION} ${NAMESPACE}/judgehost:latest && \
+docker tag ${NAMESPACE}/default-judgehost-chroot:${VERSION} ${NAMESPACE}/default-judgehost-chroot:latest && \
+docker push ${NAMESPACE}/domserver:latest && docker push ${NAMESPACE}/judgehost:latest && docker push ${NAMESPACE}/default-judgehost-chroot:latest"
 fi
