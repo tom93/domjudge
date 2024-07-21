7ff1059dc4562ae54ec7ec8c4bf28da2d387294c
Tom Levy <tomlevy93@gmail.com>
2024-07-21 23:28:14 +1200

    Import docker files
    Source:
    https://github.com/DOMjudge/domjudge-packaging/blob/master/docker
    at commit d1da7b7ff (Remove apt lists after 'apt-get update' in
    docker-contributor/Dockerfile, 2024-04-26)

---
+++ docker/judgehost/chroot-and-tar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Usage: https://github.com/DOMjudge/domjudge/blob/main/misc-tools/dj_make_chroot.in#L58-L87
+/opt/domjudge/judgehost/bin/dj_make_chroot
+
+cd /
+echo "[..] Compressing chroot"
+tar -czpf /chroot.tar.gz --exclude=/chroot/tmp --exclude=/chroot/proc --exclude=/chroot/sys --exclude=/chroot/mnt --exclude=/chroot/media --exclude=/chroot/dev --one-file-system /chroot
+echo "[..] Compressing judge"
+tar -czpf /judgehost.tar.gz /opt/domjudge/judgehost
